63a1d6c210d5febe8ac238bd7568c6516c88e9cb
Josh Malkinson <josh.malkinson@codethink.co.uk>
2015-01-21 15:38:49 +0000

    Challenge 1: name asker

---
+++ challenge01/name-asker.py
@@ -0,0 +1,5 @@
+name = raw_input("What is your name? ")
+age = raw_input("How old are you? ")
+username = raw_input("And what's your username? ")
+
+print ("Your name is " + name + ", you are " + age + " years old, and your username is " + username + ".")
